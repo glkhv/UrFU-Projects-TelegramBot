b1bc951f08f64e062997d8f7cc58de0dc6b7ac3b
DanLisow <56221679+DanLisow@users.noreply.github.com>
2020-07-17 20:58:19 +0300

    Update defproject-scene.js

--- defproject-scene.js
+++ defproject-scene.js
@@ -6,14 +6,14 @@ const WizardScene = require("telegraf/scenes/wizard");
 const mysql = require("mysql");
 const { leave } = Stage;
 
-function defprojectSceneGenerate() {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "telegraf",
-  });
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "telegraf",
+});
 
+function defprojectSceneGenerate() {
   const stepHandler = new Composer();
 
   const defproject = new WizardScene(
@@ -45,7 +45,8 @@ function defprojectSceneGenerate() {
     },
     (ctx) => {
       ctx.wizard.state.time = ctx.message.text.replace(" ", "");
-
+      const data = ctx.wizard.state.data;
+      let time = ctx.wizard.state.time;
       const regexp = /\d{2}:\d{2}/;
 
       if (!regexp.test(ctx.wizard.state.time)) {
@@ -54,31 +55,40 @@ function defprojectSceneGenerate() {
         );
         ctx.wizard.selectStep(2);
         ctx.wizard.state.flag = false;
-      }
-      // dataTime = [];
-      // connection.query(
-      //   "SELECT time FROM schedule WHERE data = 'aiml'",
-      //   (err, res) => {
-      //     if (err) throw err;
+      } else {
+        connection.query(
+          `SELECT time FROM schedule WHERE data = '${data}'`,
+          (err, res) => {
+            console.log(res);
+            if (res.length == 0) {
+              ctx.reply("Ответ записан!");
 
-      //     res.forEach((item) => {
-      //       dataTime.push(item.time.replace(":00", ""));
-      //     });
+              connection.connect(() => {
+                connection.query(
+                  `INSERT INTO schedule (id, team, data, time) VALUES (NULL, '${ctx.wizard.state.team}', '${ctx.wizard.state.data}', '${ctx.wizard.state.time}')`
+                );
+                connection.query("SET SESSION wait_timeout = 604800");
+              });
+              ctx.scene.leave();
+              console.log(ctx.wizard.state);
+            } else if (res.some((elem) => elem.time == time)) {
+              ctx.reply(
+                "К сожалению, это время уже занято\nвведите любой символ для продолжения"
+              );
+              ctx.wizard.selectStep(2);
+              ctx.wizard.state.flag = false;
+            } else {
+              ctx.reply("Ответ записан!");
 
-      //     console.log(dataTime);
-      //   }
-      // );
-      else {
-        ctx.reply("Ответ записан!");
-
-        connection.connect(() => {
-          connection.query(
-            `INSERT INTO schedule (id, team, data, time) VALUES (NULL, '${ctx.wizard.state.team}', '${ctx.wizard.state.data}', '${ctx.wizard.state.time}')`
-          );
-          connection.query("SET SESSION wait_timeout = 604800");
-        });
-        ctx.scene.leave();
-        console.log(ctx.wizard.state);
+              connection.query(
+                `INSERT INTO schedule (id, team, data, time) VALUES (NULL, '${ctx.wizard.state.team}', '${ctx.wizard.state.data}', '${ctx.wizard.state.time}')`
+              );
+              connection.query("SET SESSION wait_timeout = 604800");
+              ctx.scene.leave();
+              console.log(ctx.wizard.state);
+            }
+          }
+        );
       }
     }
   );
